ceb260e38b98ed358904275de3c8a9c156849865
mcookie47 <57654936+mcookie47@users.noreply.github.com>
2024-02-02 22:11:37 -0800

    Update pointers2.cpp

--- pointers2.cpp
+++ pointers2.cpp
@@ -4,35 +4,23 @@ using namespace std;
 int main()
 {
 	int firstvalue, secondvalue;
-	int* mypointer;
-
-	mypointer = &firstvalue;
-	*mypointer = 10;
-	mypointer = &secondvalue;
-	*mypointer = 20;
-	cout << "firstvalue is " << firstvalue << '\n';
-	cout << "secondvalue is " << secondvalue << '\n' << '\n';
-	cout << "***********************************" << '\n' << '\n';
-	
-	//new pointer
-	int thirdvalue, fourthvalue;
 	int* p1, * p2;
 
-	thirdvalue = 10, fourthvalue = 20;
-	p1 = &thirdvalue;
-	p2 = &fourthvalue;
+	firstvalue = 10, secondvalue = 20;
+	p1 = &firstvalue;
+	p2 = &secondvalue;
 
-	cout << "Third value is " << thirdvalue << '\n';
-	cout << "Fourth value is " << fourthvalue << '\n';
+	cout << "First value is " << firstvalue << '\n';
+	cout << "Second value is " << secondvalue << '\n';
 	cout << "&&" << '\n';
 	cout << "Pointer 1 is " << *p1 << '\n';
 	cout << "Pointer 2 is " << *p2 << '\n';
-	
+
 	*p1 = 35;
 	cout << "===>" << '\n';
 
-	cout << '\t' << "Third value is " << thirdvalue << '\n';
-	cout << '\t' << "Fourth value is " << fourthvalue << '\n';
+	cout << '\t' << "First value is " << firstvalue << '\n';
+	cout << '\t' << "Second value is " << secondvalue << '\n';
 	cout << '\t' << "&&" << '\n';
 	cout << '\t' << "Pointer 1 is " << *p1 << '\n';
 	cout << '\t' << "Pointer 2 is " << *p2 << '\n';
@@ -40,8 +28,8 @@ int main()
 	*p2 = *p1;
 	cout << "===>" << '\n';
 
-	cout << '\t' << "Third value is " << thirdvalue << '\n';
-	cout << '\t' << "Fourth value is " << fourthvalue << '\n';
+	cout << '\t' << "First value is " << firstvalue << '\n';
+	cout << '\t' << "Second value is " << secondvalue << '\n';
 	cout << '\t' << "&&" << '\n';
 	cout << '\t' << "Pointer 1 is " << *p1 << '\n';
 	cout << '\t' << "Pointer 2 is " << *p2 << '\n';
@@ -49,8 +37,8 @@ int main()
 	p1 = p2;
 	cout << "===>" << '\n';
 
-	cout << '\t' << "Third value is " << thirdvalue << '\n';
-	cout << '\t' << "Fourth value is " << fourthvalue << '\n';
+	cout << '\t' << "First value is " << firstvalue << '\n';
+	cout << '\t' << "Second value is " << secondvalue << '\n';
 	cout << '\t' << "&&" << '\n';
 	cout << '\t' << "Pointer 1 is " << *p1 << '\n';
 	cout << '\t' << "Pointer 2 is " << *p2 << '\n';
@@ -58,8 +46,8 @@ int main()
 	*p1 = 123;
 	cout << "===>" << '\n';
 
-	cout << '\t' << "Third value is " << thirdvalue << '\n';
-	cout << '\t' << "Fourth value is " << fourthvalue << '\n';
+	cout << '\t' << "First value is " << firstvalue << '\n';
+	cout << '\t' << "Second value is " << secondvalue << '\n';
 	cout << '\t' << "&&" << '\n';
 	cout << '\t' << "Pointer 1 is " << *p1 << '\n';
 	cout << '\t' << "Pointer 2 is " << *p2 << '\n';
